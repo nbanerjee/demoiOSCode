8b8446a5e1502e53e2c33cc960d96316580af934
Nabarun Banerjee <nbanerjee@kreeti.com>
2013-12-16 10:28:46 +0530

    Initial Commit

---
+++ NBAnimation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NBAnimation
+//
+//  Created by Nabarun Banerjee on 16/12/13.
+//  Copyright (c) 2013 Kreeti Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
