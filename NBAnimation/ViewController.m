09a33a9793dcdd348ad8e8698f68d11715eb2f79
Nabarun Banerjee <nbanerjee@kreeti.com>
2014-03-21 16:35:29 +0530

    first commit

--- NBAnimation/ViewController.m
+++ NBAnimation/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,50 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+  // Load images
+  NSArray *imageNames = @[@"win_1.png", @"win_2.png", @"win_3.png", @"win_4.png",
+                          @"win_5.png", @"win_6.png", @"win_7.png", @"win_8.png",
+                          @"win_9.png", @"win_10.png", @"win_11.png", @"win_12.png",
+                          @"win_13.png", @"win_14.png", @"win_15.png", @"win_16.png"];
+  
+  NSMutableArray *images = [[NSMutableArray alloc] init];
+  for (int i = 0; i < imageNames.count; i++) {
+    [images addObject:[UIImage imageNamed:[imageNames objectAtIndex:i]]];
+  }
+  
+  // Normal Animation
+  UIImageView *animationImageView = [[UIImageView alloc] initWithFrame:CGRectMake(60, 95, 86, 193)];
+  animationImageView.animationImages = images;
+  animationImageView.animationDuration = 0.5;
+  
+  UIImageView *slowAnimationImageView = [[UIImageView alloc] initWithFrame:CGRectMake(146, 95, 86, 193)];
+  slowAnimationImageView.animationImages = images;
+  slowAnimationImageView.animationDuration = 1.0;
+  
+  [self.view addSubview:animationImageView];
+  [animationImageView startAnimating];  
+  [self.view addSubview:slowAnimationImageView];
+  [slowAnimationImageView startAnimating];
+  
+  [self startKeyFrameAnimation];  
+}
+
+-(void)startKeyFrameAnimation{
+  CALayer *layer = [CALayer layer];
+  [layer setPosition:CGPointMake(50.0, 50.0)];
+  [layer setBounds:CGRectMake(0.0, 0.0, 50.0, 60.0)];
+  [layer setBackgroundColor:[[UIColor blueColor] CGColor]];
+  [self.view.layer addSublayer:layer];
+  
+  CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+  NSArray *times = [NSArray arrayWithObjects:[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:0.33], [NSNumber numberWithFloat:0.5], [NSNumber numberWithFloat:1.0], nil];
+  [anim setKeyTimes:times];
+  
+  NSArray *values = [NSArray arrayWithObjects:[NSValue valueWithCGPoint: CGPointMake(50., 50.)], [NSValue valueWithCGPoint: CGPointMake(50., 150.)], [NSValue valueWithCGPoint: CGPointMake(220., 150.)], [NSValue valueWithCGPoint: CGPointMake(220., 50.)], nil];
+  [anim setValues:values];
+  [anim setDuration:6.0];
+  [anim setRepeatCount:HUGE_VALF];
+  [layer addAnimation:anim forKey:@"position"];
 }
 
 - (void)didReceiveMemoryWarning
